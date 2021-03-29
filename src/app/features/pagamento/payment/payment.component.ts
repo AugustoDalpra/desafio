24d49d88acc2461f236f39bf6f0aae0fa0e0be06
augusto <augustodalpra@hotmail.com>
2021-03-29 09:34:34 -0300

    Adicionando refresh da pagina no botão de confirmar (botão ainda não tem nenuhma condição)

--- src/app/features/pagamento/payment/payment.component.ts
+++ src/app/features/pagamento/payment/payment.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+constructor( private location: Location) { }
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+
+}
+
+efetuarCompra(){
+
+
+  alert('Compra enviada!! Vá até a agência do banco mais próximo para confirmar a compra com o gerente');
+
+  this.refresh();
+
+}
+
+
+
+refresh(){
+
+  location.reload();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
